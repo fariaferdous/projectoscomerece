3c15c3bdab1d56f3a8a75c553cee5f9c6ba0ae32
ferdous00 <farianoor2@gmail.com>
2017-12-24 23:25:26 -0500

    testrunner

--- src/test/java/com/comerece/testrunner/Testngtestrunner.java
+++ src/test/java/com/comerece/testrunner/Testngtestrunner.java
@@ -3,9 +3,13 @@ package com.comerece.testrunner;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 public class Testngtestrunner{
-@CucumberOptions
-(features={"Features"}, glue={"com.comerece.stepdefs"})
- public class TestRunner extends AbstractTestNGCucumberTests{
-
-}
+	
+	
+	@CucumberOptions
+	(features="Features",
+	glue="com.comerece.stepdefs",
+	tags="@login"
+	)
+	
+	public class TestRunner extends AbstractTestNGCucumberTests{}
 }
